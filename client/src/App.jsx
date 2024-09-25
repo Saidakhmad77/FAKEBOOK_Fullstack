b3e59fc7edb2a437ba28b2e493295b19cdccb662
Saidakhmad77 <lionuz669@gmail.com>
2024-09-25 16:43:56 +0900

    added token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
 import './App.css'
 
 function App() {
   return (
     <>
     <Login></Login>
+    <Register></Register>
     </>
   )
 }
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
+import UserToken from './UserToken';
 
 
 function Login(){
@@ -7,17 +8,40 @@ function Login(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const [loginSucces, setLoginSuccess] = useState(false);
+    const [loginAttempt, setLoginAttempt] = useState(1);
+
+    const { token, removeToken, setToken } = UserToken();
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token")
+        if (storedToken) {
+            setLoginSuccess(true);
+        }
+    }, [loginAttempt]);
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post("http://127.0.0.1:5000/login", {
+            const response = await axios.post("http://127.0.0.1:5000/token", {
                 username,
                 password,
             })
+            if (response.status === 200) {
+                console.log("Login successful");
+                setToken(response.data.access_token);
+                setLoginSuccess(true)
+            } else {
+                console.log("Login failed");
+            }
             console.log(response.status);
         } catch (error) {
             console.log(error)
+        } finally {
+            setLoginAttempt(loginAttempt + 1)
+            console.log("Logins attempted: " + loginAttempt)
         }
     };
 
@@ -25,6 +49,7 @@ function Login(){
         <>
         <div className='login-form-container'>
             <h2>Login</h2>
+            {loginSucces && <h1>LOGIN SUCCESSFUL</h1>}
             <form onSubmit={handleSubmit}>
                 <div className='input-container'>
                     <input 
